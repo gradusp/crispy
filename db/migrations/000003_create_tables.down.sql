2ead02a248266282c292472a8b0b55a9b40e6ab0
Pavel Fiskovich <pavel.fiskovich@gmail.com>
2021-01-11 06:03:16 +0300

    Added db migrations

---
+++ db/migrations/000003_create_tables.down.sql
@@ -0,0 +1,5 @@
+drop table if exists controller.healthchecks;
+drop table if exists controller.reals;
+drop table if exists controller.balancing_services;
+drop table if exists controller.clusters;
+drop table if exists controller.security_zones;
---
+++ db/migrations/000003_create_tables.up.sql
@@ -0,0 +1,60 @@
+create table if not exists controller.security_zones
+(
+    id   uuid default gen_random_uuid(),
+    name text,
+    primary key (id),
+    unique (name)
+);
+
+create table if not exists controller.clusters
+(
+    id               uuid default gen_random_uuid(),
+    name             text not null,
+    security_zone_id uuid not null,
+    capacity         int  not null,
+    primary key (id),
+    unique (name),
+    foreign key (security_zone_id) references controller.security_zones (id)
+);
+
+create table if not exists controller.balancing_services
+(
+    id             uuid default gen_random_uuid(),
+    cluster_id     uuid not null,
+    routing_type   text not null,
+    balancing_type text not null,
+    bandwidth      int  not null,
+    proto          text not null,
+    addr           inet not null,
+    port           int  not null,
+    primary key (id),
+    unique (proto, addr, port),
+    foreign key (cluster_id) references controller.clusters (id)
+);
+
+create table if not exists controller.reals
+(
+    id                   uuid default gen_random_uuid(),
+    balancing_service_id uuid not null,
+    addr                 inet not null,
+    port                 int  not null,
+    hc_addr              text,
+    primary key (id),
+    unique (addr, port),
+    foreign key (balancing_service_id) references controller.balancing_services (id) on delete cascade
+);
+
+create table if not exists controller.healthchecks
+(
+    id                   uuid default gen_random_uuid(),
+    balancing_service_id uuid,
+    hello_timer          int,
+    response_timer       int,
+    alive_threshold      int,
+    dead_threshold       int,
+    quorum               int,
+    hysteresis           int,
+    primary key (id),
+    foreign key (balancing_service_id) references controller.balancing_services (id) on delete cascade
+);
+

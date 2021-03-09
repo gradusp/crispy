ce24d26e0a91c26f41adfec101153e9d68cd5ccc
Pavel Fiskovich <pavel.fiskovich@gmail.com>
2021-03-09 14:30:26 +0300

    Model refactored: Real

--- internal/model/Real.go
+++ internal/model/Real.go
@@ -3,11 +3,8 @@ package model
 import "net"
 
 type Real struct {
-	ID              string   `json:"id"`
-	Addr            net.IP   `json:"addr"`
-	Port            int      `json:"port"`
-	HealthcheckAddr net.IP   `json:"hcAddr"`
-	HealthcheckPort int      `json:"hcPort"`
-	ServiceID       string   `json:"serviceId,omitempty"`
-	Service         *Service `json:"service,omitempty"`
+	ID        string `json:"id"`
+	ServiceID string `json:"serviceId"`
+	Addr      net.IP `json:"addr"`
+	Port      int    `json:"port"`
 }

f658ae3eccf633b9e819f19c0b0fecb0da042175
Pavel Fiskovich <pavel.fiskovich@gmail.com>
2021-03-09 14:29:38 +0300

    Model refactored: Service

--- internal/model/Service.go
+++ internal/model/Service.go
@@ -3,16 +3,15 @@ package model
 import "net"
 
 type Service struct {
-	ID            string `json:"id"`
-	BalancingType string `json:"balancingType"`
-	RoutingType   string `json:"routingType"`
-	Bandwidth     int    `json:"bandwidth"`
-	Proto         string `json:"proto"`
-	Addr          net.IP `json:"addr"`
-	Port          int    `json:"port"`
-	ClusterID     string `json:"clusterId,omitempty"`
-	//Cluster       Cluster        `json:"cluster,omitempty"`
-	Reals        []*Real        `json:"reals"`
-	Healthchecks []*Healthcheck `json:"healthchecks"`
-	//Orders        []Order  `pg:"many2many:controller.order_to_balancing_service"`
+	Observable    `json:"-"`
+	ClusterID     string  `json:"clusterId"`
+	ID            string  `json:"id"`
+	BalancingType string  `json:"balancingType"`
+	RoutingType   string  `json:"routingType"`
+	Bandwidth     int     `json:"bandwidth"`
+	Proto         string  `json:"proto"`
+	Addr          net.IP  `json:"addr"`
+	Port          int     `json:"port"`
+	Reals         []*Real `json:"reals"`
+	*Healthchecks `json:"healthchecks"`
 }
